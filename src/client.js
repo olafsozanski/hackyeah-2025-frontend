19e92b37432d5b2d6cb09daf3f302b066f5e34b1
olafsozanski <olaf.sozanski@gmail.com>
2025-10-04 19:12:07 +0200

    update client

--- src/client.js
+++ src/client.js
@@ -1,5 +1,18 @@
 import {Axios} from 'axios';
 
 export const client = new Axios({
-    baseURL: 'http://localhost:8000/',
+    baseURL: 'http://localhost:3000/',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
+
+client.interceptors.request.use((config) => {
+    config.data = JSON.stringify(config.data);
+    return config;
+});
+
+client.interceptors.response.use((response) => {
+    response.data = JSON.parse(response.data);
+    return response;
 });
